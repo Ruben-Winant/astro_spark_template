d7076de2f99ee300ee9a25f3882d10c302b15fac
Ruben winant <ruben.winant@hotmail.com>
2024-09-21 21:32:29 +0200

    prod ready???

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -62,6 +62,47 @@ export const ui = {
     "slider.card_5_description":
       "Astro's sitemap integration helps search engines discover your content, improving your site's SEO effortlessly.",
     "features.title": "Features",
+    "components.description":
+      "Here you'll find a collection of elements, icons and such that are provided by this theme.",
+    "test.title": "Sample Card",
+    "test.description":
+      "This is a placeholder card to demonstrate the basic structure and functionality of an element. You can customize the content and design to suit your specific needs.",
+    "test.iconTitle": "Icons",
+    "test.iconDescription":
+      "Hover over each icon to see it's name. All icons are svg files within the public/icons directory.",
+    "test.dropdownCardTitle": "Dropdown cards",
+    "test.dropdownCardDescription": "Powered by alpine.js",
+    "test.fullWidthBannerTitle": "Full width banner",
+    "test.fullWidthBannerDescription":
+      "A full width banner with an optional cta button",
+    "test.button": "click here",
+    "test.autoSliderTitle": "Auto slider with TextCards",
+    "test.autoSliderDescription":
+      "A no js, css only auto slider where you can pause the animation by hovering over a card. An optional title and description can also be provided.",
+    "test.containersTitle": "Containers",
+    "test.cardGridDescription":
+      "The CardGrid component allows flexible content layout. Provide content through either the 'upper-row' or 'lower-row' slot, and the component will automatically distribute it into columns.",
+    "test.cardGridTitle": "What's included",
+    "test.cardGridTitle1": "Contact page",
+    "test.cardGridDescription1":
+      "Capture leads and stay connected with a user-friendly contact form, powered by mailersend.",
+    "test.cardGridTitle2": "Ready-to-go blog",
+    "test.cardGridDescription2":
+      "Start blogging using the I18N compatible, MDX powered blog!",
+    "test.cardGridTitle3": "Easy setup",
+    "test.cardGridDescription3":
+      "Get up and running quickly using our premade pages and components.",
+    "test.cardGridTitle4": "FAQ page",
+    "test.cardGridDescription4":
+      "People might have questions, now you can answer them!",
+    "test.cardGridTitle5": "Speak Their Language",
+    "test.cardGridDescription5": "I18N built in. Convert more!",
+    "test.cardsTitle": "Cards",
+    "test.cardsDescription": "Some of the premade card components, some of t",
+    "test.cardClass":
+      "This CSS class .card is designed for a flexible, visually appealing card component with various styles and hover effects.",
+    "test.iconCardDescription":
+      "A responsive, interactive list item (<li>) that functions as an icon card with an image and title, using Tailwind CSS for styling.",
   },
   nl: {
     "layout.test": "hallo",
@@ -127,6 +168,48 @@ export const ui = {
     "slider.card_5_title": "SEO geoptimaliseerd",
     "slider.card_5_description":
       "Astro's sitemap-integratie helpt zoekmachines uw inhoud te ontdekken, waardoor de SEO van uw site moeiteloos wordt verbeterd.",
+    "components.description":
+      "Hier vindt u een verzameling elementen, pictogrammen en dergelijke die door dit thema worden geleverd.",
+    "test.title": "Voorbeeldkaart",
+    "test.description":
+      "Dit is een tijdelijke kaart om de basisstructuur en functionaliteit van een element te demonstreren. U kunt de inhoud en het ontwerp aanpassen aan uw specifieke behoeften.",
+    "test.iconTitle": "Pictogrammen",
+    "test.iconDescription":
+      "Houd de muiscursor boven elk pictogram om de naam te zien. Alle pictogrammen zijn svg-bestanden in de map public/icons.",
+    "test.dropdownCardTitle": "Dropdown-kaarten",
+    "test.dropdownCardDescription": "Aangedreven door alpine.js",
+    "test.fullWidthBannerTitle": "Banner op volledige breedte",
+    "test.fullWidthBannerDescription":
+      "Een banner op volledige breedte met een optionele call-to-action-knop",
+    "test.button": "Klik hier",
+    "test.autoSliderTitle": "Automatische schuifregelaar met tekstkaarten",
+    "test.autoSliderDescription":
+      "Een automatische schuifregelaar zonder JavaScript, alleen met CSS, waarbij u de animatie kunt pauzeren door met de muis over een kaart te gaan. Een optionele titel en beschrijving kunnen ook worden opgegeven.",
+    "test.containersTitle": "Containers",
+    "test.cardGridDescription":
+      "De CardGrid-component biedt een flexibele indeling van de inhoud. Voeg inhoud toe via de 'upper-row' of 'lower-row'-slot, en de component zal deze automatisch in kolommen verdelen.",
+    "test.cardGridTitle": "Wat is inbegrepen",
+    "test.cardGridTitle1": "Contactpagina",
+    "test.cardGridDescription1":
+      "Verzamel leads en blijf verbonden met een gebruiksvriendelijk contactformulier, aangedreven door mailersend.",
+    "test.cardGridTitle2": "Klaar-om-te-gaan blog",
+    "test.cardGridDescription2":
+      "Begin met bloggen met behulp van de I18N-compatibele, MDX-aangedreven blog!",
+    "test.cardGridTitle3": "Eenvoudige installatie",
+    "test.cardGridDescription3":
+      "Kom snel aan de slag met onze vooraf gemaakte pagina's en componenten.",
+    "test.cardGridTitle4": "FAQ-pagina",
+    "test.cardGridDescription4":
+      "Mensen hebben misschien vragen, nu kunt u ze beantwoorden!",
+    "test.cardGridTitle5": "Spreek hun taal",
+    "test.cardGridDescription5": "I18N ingebouwd. Converteer meer!",
+    "test.cardsTitle": "Kaarten",
+    "test.cardsDescription":
+      "Enkele van de vooraf gemaakte kaartcomponenten, enkele van t",
+    "test.cardClass":
+      "Deze CSS-klasse .card is ontworpen voor een flexibele, visueel aantrekkelijke kaartcomponent met verschillende stijlen en zweveffecten.",
+    "test.iconCardDescription":
+      "Een responsief, interactief lijst item (<li>) dat functioneert als een pictogramkaart met een afbeelding en titel, met behulp van Tailwind CSS voor styling.",
   },
   fr: {
     "layout.test": "bonjour",
@@ -193,5 +276,47 @@ export const ui = {
     "slider.card_5_title": "SEO optimized",
     "slider.card_5_description":
       "L'intégration du sitemap d'Astro aide les moteurs de recherche à découvrir votre contenu, améliorant ainsi sans effort le référencement de votre site.",
+    "components.description":
+      "Vous trouverez ici une collection d'éléments, d'icônes et autres qui sont fournis par ce thème.",
+    "test.title": "Sample Card",
+    "test.description":
+      "This is a placeholder card to demonstrate the basic structure and functionality of an element. You can customize the content and design to suit your specific needs.",
+    "test.iconTitle": "Icônes",
+    "test.iconDescription":
+      "Survolez chaque icône pour voir son nom. Toutes les icônes sont des fichiers svg dans le répertoire public/icons.",
+    "test.dropdownCardTitle": "Cartes déroulantes",
+    "test.dropdownCardDescription": "Alimenté par alpine.js",
+    "test.fullWidthBannerTitle": "Bannière pleine largeur",
+    "test.fullWidthBannerDescription":
+      "Une bannière pleine largeur avec un bouton d'appel à l'action optionnel",
+    "test.button": "Cliquez ici",
+    "test.autoSliderTitle": "Curseur automatique avec cartes de texte",
+    "test.autoSliderDescription":
+      "Un curseur automatique sans JavaScript, uniquement avec CSS, où vous pouvez mettre en pause l'animation en survolant une carte. Un titre et une description facultatifs peuvent également être fournis.",
+    "test.containersTitle": "Conteneurs",
+    "test.cardGridDescription":
+      "Le composant CardGrid permet une disposition flexible du contenu. Fournissez du contenu via le slot 'upper-row' ou 'lower-row', et le composant le distribuera automatiquement en colonnes.",
+    "test.cardGridTitle": "Ce qui est inclus",
+    "test.cardGridTitle1": "Page de contact",
+    "test.cardGridDescription1":
+      "Capturez des leads et restez connecté avec un formulaire de contact convivial, alimenté par mailersend.",
+    "test.cardGridTitle2": "Blog prêt à l'emploi",
+    "test.cardGridDescription2":
+      "Commencez à bloguer en utilisant le blog compatible I18N, alimenté par MDX !",
+    "test.cardGridTitle3": "Installation facile",
+    "test.cardGridDescription3":
+      "Soyez opérationnel rapidement en utilisant nos pages et composants pré-faits.",
+    "test.cardGridTitle4": "Page FAQ",
+    "test.cardGridDescription4":
+      "Les gens peuvent avoir des questions, maintenant vous pouvez y répondre !",
+    "test.cardGridTitle5": "Parlez leur langue",
+    "test.cardGridDescription5": "I18N intégré. Convertissez plus !",
+    "test.cardsTitle": "Cartes",
+    "test.cardsDescription":
+      "Quelques-uns des composants de carte pré-faits, quelques-uns de t",
+    "test.cardClass":
+      "Cette classe CSS .card est conçue pour un composant de carte flexible et visuellement attrayant avec différents styles et effets de survol.",
+    "test.iconCardDescription":
+      "Un élément de liste (<li>) réactif et interactif qui fonctionne comme une carte d'icône avec une image et un titre, en utilisant Tailwind CSS pour le style.",
   },
 };
